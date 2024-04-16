ce5d448c65b4ac7caef8090123188a6d797ef1cf
DRC Lab <85211068+DRCRecoveryData@users.noreply.github.com>
2024-04-16 21:04:55 +0700

    Update neofighter.ino
    Adding led pallete to work like midi fighter with color with velocity

--- firmware/neofighter.ino
+++ firmware/neofighter.ino
@@ -5,22 +5,15 @@
 #define NUM_PXL 32
 #define debounceDelay 5
 
-//button states for debouncing
-int state = 0;
-int prev_state = 0;
-unsigned long lastDebounceTime = 0;
+// Color palette from the first code
+const byte _R[128] = {0, 61, 125, 190, 255, 255, 61, 125, 190, 255, 255, 61, 125, 190, 255, 255, 61, 125, 190, 255, 255, 61, 125, 190, 255, 255, 45, 93, 142, 223, 190, 28, 61, 93, 190, 125, 12, 28, 45, 158, 61, 0, 0, 0, 125, 0, 0, 0, 0, 125, 0, 0, 0, 0, 125, 0, 0, 0, 0, 125, 0, 0, 0, 0, 125, 0, 0, 0, 0, 125, 0, 0, 0, 0, 125, 0, 0, 0, 0, 125, 0, 0, 0, 0, 125, 0, 12, 28, 45, 158, 61, 28, 61, 93, 190, 125, 45, 93, 142, 223, 190, 61, 125, 190, 255, 255, 61, 125, 190, 255, 255, 61, 125, 190, 255, 255, 61, 125, 190, 255, 255, 36, 73, 109, 146, 182, 219, 255};
+const byte _G[128] = {0, 0, 0, 0, 125, 0, 12, 28, 45, 158, 61, 28, 61, 93, 190, 125, 45, 93, 142, 223, 190, 61, 125, 190, 255, 255, 61, 125, 190, 255, 255, 61, 125, 190, 255, 255, 61, 125, 190, 255, 255, 61, 125, 190, 255, 255, 61, 125, 190, 255, 255, 61, 125, 190, 255, 255, 61, 125, 190, 255, 255, 61, 125, 190, 255, 255, 45, 93, 142, 223, 190, 28, 61, 93, 190, 125, 12, 28, 45, 158, 61, 0, 0, 0, 125, 0, 0, 0, 0, 125, 0, 0, 0, 0, 125, 0, 0, 0, 0, 125, 0, 0, 0, 0, 125, 0, 0, 0, 0, 125, 0, 0, 0, 0, 125, 0, 0, 0, 0, 125, 0, 36, 73, 109, 146, 182, 219, 255};
+const byte _B[128] = {0, 0, 0, 0, 125, 0, 0, 0, 0, 125, 0, 0, 0, 0, 125, 0, 0, 0, 0, 125, 0, 0, 0, 0, 125, 0, 0, 0, 0, 125, 0, 0, 0, 0, 125, 0, 0, 0, 0, 125, 0, 0, 0, 0, 125, 0, 12, 28, 45, 158, 61, 28, 61, 93, 190, 125, 45, 93, 142, 223, 190, 61, 125, 190, 255, 255, 61, 125, 190, 255, 255, 61, 125, 190, 255, 255, 61, 125, 190, 255, 255, 61, 125, 190, 255, 255, 61, 125, 190, 255, 255, 61, 125, 190, 255, 255, 61, 125, 190, 255, 255, 61, 125, 190, 255, 255, 45, 93, 142, 223, 190, 28, 61, 93, 190, 125, 12, 28, 45, 158, 61, 36, 73, 109, 146, 182, 219, 255};
 
-//keep button states so we're only sending midi signals when there's a change
+// Keep button states so we're only sending MIDI signals when there's a change
 int previousButtons = 0;
 
-
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_PXL, NEO_PIN, NEO_GRB + NEO_KHZ800);
-
-// First parameter is the event type (0x09 = note on, 0x08 = note off).
-// Second parameter is note-on/note-off, combined with the channel.
-// Channel can be anything between 0-15. Typically reported to the user as 1-16.
-// Third parameter is the note number (48 = middle C).
-// Fourth parameter is the velocity (64 = normal, 127 = fastest).
 
 void noteOn(byte channel, byte pitch, byte velocity) {
   midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
@@ -32,42 +25,18 @@ void noteOff(byte channel, byte pitch, byte velocity) {
   MidiUSB.sendMIDI(noteOff);
 }
 
-// First parameter is the event type (0x0B = control change).
-// Second parameter is the event type, combined with the channel.
-// Third parameter is the control number number (0-119).
-// Fourth parameter is the control value (0-127).
-
-void controlChange(byte channel, byte control, byte value) {
-  midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
-  MidiUSB.sendMIDI(event);
-}
-
-
 void setPixels(int pinStates){
   for(int i=0; i<16; i++){
-    if(bitRead(pinStates, i)){ //button down
-      pixels.setPixelColor((i*2), pixels.Color(127,127,127)); //white
-      pixels.setPixelColor((i*2)+1, pixels.Color(127,127,127));
-      
-    }else{ //button up
-      pixels.setPixelColor((i*2), pixels.Color(30,10,10)); //pink
-      pixels.setPixelColor((i*2)+1, pixels.Color(30,10,10));      
-    }
-    //
-    if (bitRead(pinStates, i) != bitRead(previousButtons, i))
-    {
-      if (bitRead(pinStates, i))
-      {
-        bitWrite(previousButtons, i , 1);
-        noteOn(0, (40+i), 127); //Note range 40-56
-        MidiUSB.flush();
-      }
-      else
-      {
-        bitWrite(previousButtons, i , 0);
-        noteOff(0, (40+i), 0);
-        MidiUSB.flush();
-      }
+    if(bitRead(pinStates, i)){ // Button down
+      byte velocity = 127; // Maximum velocity when button is pressed
+      noteOn(0, (36+i), velocity); // Send MIDI note on
+      pixels.setPixelColor((i*2), pixels.Color(_R[velocity], _G[velocity], _B[velocity])); // Set LED color based on velocity
+      pixels.setPixelColor((i*2)+1, pixels.Color(_R[velocity], _G[velocity], _B[velocity]));
+    } else { // Button up
+      byte velocity = 0; // Velocity 0 when button is released
+      noteOff(0, (36+i), velocity); // Send MIDI note off
+      pixels.setPixelColor((i*2), pixels.Color(_R[velocity], _G[velocity], _B[velocity])); // Set LED color based on velocity
+      pixels.setPixelColor((i*2)+1, pixels.Color(_R[velocity], _G[velocity], _B[velocity]));
     }
   }
   pixels.show();
@@ -75,11 +44,7 @@ void setPixels(int pinStates){
 
 void setup() {
 
-  /* Pin Configuration
-   * btn: 1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16
-   * pin: D0  B7  F4  F5  D1  D5  F7  F6  D6  D4  C7  C6  B4  D7  B6  B5
-   */
-    
+  // Set pins for input
   DDRB  &= ~(1<<7 | 1<<6 | 1<< 5 | 1<<4);
   PORTB |=  (1<<7 | 1<<6 | 1<< 5 | 1<<4);
   DDRC  &= ~(1<<7 | 1<<6);
@@ -91,7 +56,6 @@ void setup() {
 
   pixels.begin();
   Serial.begin(115200);
-  
 }
 
 void loop(){
@@ -113,18 +77,9 @@ void loop(){
                     (PINB&(1<<6) ? 0 : (1<<14)) |
                     (PINB&(1<<5) ? 0 : (1<<15));
 
-  if (pinStates != prev_state) {
-    // reset the debouncing timer
-    lastDebounceTime = millis();
+  // Check for button state changes
+  if (pinStates != previousButtons) {
+    setPixels(pinStates);
+    previousButtons = pinStates;
   }
-  
-  if((millis()-lastDebounceTime) > debounceDelay){
-    if(pinStates !=state){
-      state = pinStates;
-      setPixels(pinStates);
-    }
-  }
-
-  prev_state = pinStates;  
-
 }
